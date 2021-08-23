ff3b4973af59f151b801cae7f6444a7390ec2ff2
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-08-23 02:47:48 +0200

    add test case for Currencies mapping export

--- test/index.js
+++ test/index.js
@@ -1,32 +1,51 @@
-import test from "ava";
-import { getCurrency } from "../package/index";
+import test from 'ava';
+import { getCurrency, Currencies } from '../package/index';
 
-test("Country code", (t) => {
-  t.is(getCurrency("SE"), "SEK");
-  t.is(getCurrency("Nl"), "EUR");
-  t.is(getCurrency("dE"), "EUR");
-  t.is(getCurrency("us"), "USD");
-  t.is(getCurrency("AU"), "AUD");
+test('Country code', (t) => {
+
+  t.is(getCurrency('SE'), 'SEK');
+  t.is(getCurrency('Nl'), 'EUR');
+  t.is(getCurrency('dE'), 'EUR');
+  t.is(getCurrency('us'), 'USD');
+  t.is(getCurrency('AU'), 'AUD');
 
   t.pass();
+
 });
 
-test("Country code in uppercase", (t) => {
-  t.is(getCurrency("SE"), "SEK");
-  t.is(getCurrency("NL"), "EUR");
-  t.is(getCurrency("DE"), "EUR");
-  t.is(getCurrency("US"), "USD");
-  t.is(getCurrency("AU"), "AUD");
+test('Country code in uppercase', (t) => {
+
+  t.is(getCurrency('SE'), 'SEK');
+  t.is(getCurrency('NL'), 'EUR');
+  t.is(getCurrency('DE'), 'EUR');
+  t.is(getCurrency('US'), 'USD');
+  t.is(getCurrency('AU'), 'AUD');
 
   t.pass();
+
 });
 
-test("Country code in lowercase", (t) => {
-  t.is(getCurrency("se"), "SEK");
-  t.is(getCurrency("nl"), "EUR");
-  t.is(getCurrency("de"), "EUR");
-  t.is(getCurrency("us"), "USD");
-  t.is(getCurrency("au"), "AUD");
+test('Country code in lowercase', (t) => {
+
+  t.is(getCurrency('se'), 'SEK');
+  t.is(getCurrency('nl'), 'EUR');
+  t.is(getCurrency('de'), 'EUR');
+  t.is(getCurrency('us'), 'USD');
+  t.is(getCurrency('au'), 'AUD');
 
   t.pass();
+
+});
+
+test('Currencies exposed mapping export', (t) => {
+
+  t.is(Currencies.SE, 'SEK');
+  t.is(Currencies.NL, 'EUR');
+  t.is(Currencies.DE, 'EUR');
+  t.is(Currencies.US, 'USD');
+  t.is(Currencies.AU, 'AUD');
+  t.is(Currencies.RU, 'RUB');
+
+  t.pass();
+
 });
